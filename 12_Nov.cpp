541bc0a9523c91341da3842042af413441979218
Babita1947 <130829271+Babita1947@users.noreply.github.com>
2023-11-12 10:50:48 +0530

    added program

---
+++ 12_Nov.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maximumStrongPairXor(vector<int>& nums) {
+        int maxi=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            for(int j=i;j<nums.size();j++)
+            {
+               if(abs(nums[i]-nums[j])<=min(nums[i],nums[j])){
+                   int xorr=nums[i]^nums[j];
+                   maxi=max(maxi,xorr);
+                   }
+                   
+            }
+        }
+        return maxi;
+    }
+};
