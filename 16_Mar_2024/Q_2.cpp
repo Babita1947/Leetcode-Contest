59a213c655b1e32d0ee96e14608c0434f3060175
Babita1947 <130829271+Babita1947@users.noreply.github.com>
2024-03-20 22:26:14 +0530

    added Q_2.cpp

--- 16_Mar_2024/Q_2.cpp
+++ 16_Mar_2024/Q_2.cpp
@@ -3,47 +3,79 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// class Solution {
+//     int minimum_Index(vector<int> &nums){
+//         int n = nums.size();
+//         int minIndex = -1;
+//         int minVal = INT_MAX;
+//         for(int i=0;i<nums.size();i++){
+//             if(nums[i] != -1 && nums[i] < minVal){
+//                 minVal = nums[i];
+//                 minIndex = i;
+//             }
+//         }
+//         return minIndex;
+//     }
+// public:
+//     vector<long long> unmarkedSumArray(vector<int>& nums, vector<vector<int>>& queries) {
+//         vector<long long> ans;
+//         int sum = 0;
+
+//         for(auto it:nums)
+//            sum += it;
+        
+//         for(int i=0;i<queries.size();i++){
+//             if(nums[queries[i][0]] != -1){
+//                 sum -= nums[queries[i][0]];
+//                 nums[queries[i][0]] = -1;
+//             }
+//             while(queries[i][1]){
+//                 int  minIndex = minimum_Index(nums);
+                
+//                 if(minIndex != -1 && nums[minIndex] != -1){
+//                     sum -= nums[minIndex];
+//                     nums[minIndex] = -1;
+//                 }
+//                 queries[i][1]--;
+//             }
+//             ans.push_back(sum);
+//         }
+//         return ans;
+//     }
+// };
+
+
 class Solution {
-    int minimum_Index(vector<int> &nums){
-        int n = nums.size();
-        int minIndex = -1;
-        int minVal = INT_MAX;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i] != -1 && nums[i] < minVal){
-                minVal = nums[i];
-                minIndex = i;
-            }
-        }
-        return minIndex;
-    }
+    
 public:
     vector<long long> unmarkedSumArray(vector<int>& nums, vector<vector<int>>& queries) {
         vector<long long> ans;
-        int sum = 0;
-
-        for(auto it:nums)
-           sum += it;
-        
+        long long sum = 0;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        for(int i=0;i<nums.size();i++){
+           pq.push({nums[i],i});
+           sum += nums[i];
+        }
+           
         for(int i=0;i<queries.size();i++){
             if(nums[queries[i][0]] != -1){
                 sum -= nums[queries[i][0]];
                 nums[queries[i][0]] = -1;
             }
-            while(queries[i][1]){
-                int  minIndex = minimum_Index(nums);
-                
-                if(minIndex != -1 && nums[minIndex] != -1){
-                    sum -= nums[minIndex];
-                    nums[minIndex] = -1;
+            while(queries[i][1] && !pq.empty()){
+                pair<int,int> min_element = pq.top();
+                pq.pop();
+                if(nums[min_element.second] != -1){
+                    sum -= nums[min_element.second];
+                    nums[min_element.second] = -1;
+                    queries[i][1]--;
                 }
-                queries[i][1]--;
             }
             ans.push_back(sum);
         }
         return ans;
     }
 };
-
 int main(){
     Solution s;
     vector<int> nums{1,2,2,1,2,3,1};
